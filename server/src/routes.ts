925140b4994ff5a7530eba138359dcda451cf3c7
Fillipe Romero <fillipe.romero@gmail.com>
2023-01-17 19:24:26 -0700

    add create habit route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,25 @@
 import { FastifyInstance } from 'fastify'
+import dayjs from 'dayjs'
+import { z } from 'zod'
 
 import { prisma } from "./lib/prisma"
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get('/getHabit', async () => {
-    return prisma.habit.findMany()
+  app.post('/habits', async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekdays: z.array(z.number().min(0).max(6))
+    })
+    const { title, weekdays } = createHabitBody.parse(request.body)
+
+    const today = dayjs().startOf('day').toDate()
+
+    return prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: { create: weekdays.map(weekDay => ({ week_day: weekDay })) }
+      }
+    })
   })
 }
